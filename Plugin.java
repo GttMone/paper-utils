ee53ddbccdee3afc8dab8b88b4d0bebae1a93d37
Vito Gos <davidgospodinov10@gmail.com>
2024-11-21 19:20:44 +0200

    Updated param

--- Plugin.java
+++ Plugin.java
@@ -2,12 +2,11 @@ package yourpackage.utils;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-import site.gttmone.cowCannon.CowCannon;
 
 public abstract class Plugin {
     private static JavaPlugin plugin;
 
-    public static void init(@NotNull CowCannon plugin) {
+    public static void init(@NotNull JavaPlugin plugin) {
         if (Plugin.plugin != null) return;
         Plugin.plugin = plugin;
     }
