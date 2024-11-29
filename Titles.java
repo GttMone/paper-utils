501364418f3a133e045fc39d8e2d962394a7ea25
Vito Gos <davidgospodinov10@gmail.com>
2024-11-29 21:53:33 +0200

    Added method for player titles

--- Titles.java
+++ Titles.java
@@ -3,6 +3,7 @@ package yourpackage.utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
@@ -14,6 +15,18 @@ public abstract class Titles {
 
     public static void showServerTitle(@NotNull String miniMessageTitle, long fadeIMilis, long stayMilis, long fadeOutMilis) {
         Bukkit.getServer().showTitle(
+                Title.title(Components.mm(miniMessageTitle),
+                        Component.empty(),
+                        Title.Times.times(Duration.ofMillis(fadeIMilis), Duration.ofMillis(stayMilis), Duration.ofMillis(fadeOutMilis)))
+        );
+    }
+
+    public static void showTitle(@NotNull Player player, @NotNull String miniMessageTitle) {
+        player.showTitle(Title.title(Components.mm(miniMessageTitle), Component.empty()));
+    }
+
+    public static void showTitle(@NotNull Player player, @NotNull String miniMessageTitle, long fadeIMilis, long stayMilis, long fadeOutMilis) {
+        player.showTitle(
                 Title.title(Components.mm(miniMessageTitle),
                         Component.empty(),
                         Title.Times.times(Duration.ofMillis(fadeIMilis), Duration.ofMillis(stayMilis), Duration.ofMillis(fadeOutMilis)))
