42cf0a761574cce13099142728d7141b1f65dd6f
Vito Gos <davidgospodinov10@gmail.com>
2024-12-14 00:33:40 +0200

    Updated logger to log all variable types

--- Logger.java
+++ Logger.java
@@ -5,11 +5,11 @@ import org.jetbrains.annotations.NotNull;
 import java.util.logging.Level;
 
 public abstract class Logger {
-    public static void log(@NotNull String message) {
-        Plugin.getPlugin().getLogger().log(Level.INFO, message);
+    public static void log(@NotNull Object message) {
+        Plugin.getPlugin().getLogger().log(Level.INFO, String.valueOf(message));
     }
 
-    public static void log(@NotNull String message, @NotNull Level type) {
-        Plugin.getPlugin().getLogger().log(type, message);
+    public static void log(@NotNull Object message, @NotNull Level type) {
+        Plugin.getPlugin().getLogger().log(type, String.valueOf(message));
     }
 }
